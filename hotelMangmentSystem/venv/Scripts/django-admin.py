be0e55ee15b59951285ef8071641716c9bdc1152
amir rami <amir.r.zarif@hotmail.com>
2018-10-25 13:46:20 +0200

    project files 2

--- hotelMangmentSystem/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\miro\Downloads\work\SE218G04\hotelMangmentSystem\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
